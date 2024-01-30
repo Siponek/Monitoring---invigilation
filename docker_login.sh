1bb39bf2c99a8d9d60bc98bfc77cdb6ee7bdd31a
Szymon Zinkowicz <sz281@nq-dev-01.iasonltd.com>
2024-01-30 17:31:56 +0100

    feat: Added checks for env files

--- docker_login.sh
+++ docker_login.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+files=("docker.env" "docker_login.env" "pipeline.env" "grafana.env")
+echo "Checking for required files"
+for file in "${files[@]}"; do
+    if [ -f "./env/$file" ]; then
+        echo "$file found"
+    else
+        echo "$file not found"
+        exit 1
+    fi
+done
 # Source the .env file
 if [ -f ./env/docker_login.env ]; then
     export $(cat ./env/docker_login.env | sed 's/#.*//g' | xargs)
     echo "$IASON_REGISTRY_PASSWORD" | docker login "$IASON_REGISTRY" --username "$IASON_REGISTRY_USER" --password-stdin
 else
-    echo ".env file not found"
+    echo "docker_login.env file not found"
     exit 1
 fi
 
